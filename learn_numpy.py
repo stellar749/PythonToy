24b82fd1d4edfbe7a12bc1f109c9d6035ba7bf29
li <1016876749@qq.com>
2020-07-11 23:43:16 +0800

    learn more numpy

--- learn_numpy.py
+++ learn_numpy.py
@@ -14,7 +14,7 @@ my_array = np.array([[4, 5], [6, 1]])
 print(my_array[0][1]) 
 print(my_array.shape) 
 my_array_column_2 = my_array[:, 1] 
-print(my_array_column_2) # 取出一行或一列，输出是list
+print(my_array_column_2) # 取出一行或一列，输出是array
 
 a = np.array([[1.0, 2.0], [3.0, 4.0]]) 
 b = np.array([[5.0, 6.0], [7.0, 8.0]]) 
@@ -29,6 +29,25 @@ vv = np.array([[2,1,3]]).T #转置的另外一种表示方法
 print(vv)
 vvv = np.array([2,1,3]).T #一维array无法转置
 print(vvv)
+
+#array运算一些特点
+print('calculate')
+a = np.array([[1, 1],[1, 1]]) #二维数组和一维数组矩阵乘法得到 [4 4]
+e = np.array([1, 3]) 
+res = a.dot(e)
+print(res)
+f = np.array([[0.6, 1]]) #二维数组和一维数组矩阵乘法得到 [5]
+g = np.array([5, 2])
+ress = f.dot(g)
+print(ress)
+h = np.array([1,2])
+resss = g.dot(h)
+print(resss) #两个一维数组矩阵乘法得到 9， 不同维度一维数组不能相乘
+j = np.array([2])
+print(1-j) #会自动转化为 [-1]
+print(f.T.dot(j)) #得到 [1.2 2]
+
+
 
 #矩阵/数组/列表转换
 print('from list')
@@ -58,6 +77,10 @@ col_1 = my_array1[0,:] #一维的ndarray
 col_2 = my_array2[0,:] #矩阵
 print(col_1)
 print(col_2)
+#行数组赋值给二维数组的列
+plus = np.array([-1, -1])
+my_array1[:,0] = plus
+print(my_array1)
 
 
 
